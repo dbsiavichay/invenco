30822c7d4aa98919d25f8c9048de566bb3830a1a
Denis Siavichay <dbsiavichay@gmail.com>
2015-09-18 10:03:03 -0500

    Admin of module equipment

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Trademark, Type, Model, Device
 
-# Register your models here.
+class TrademarkAdmin(admin.ModelAdmin):
+	list_display = ('name',)
+	search_fields = ('name',)
+
+class TypeAdmin(admin.ModelAdmin):
+	list_display = ('name', 'specifications',)
+	search_fields = ('name',)
+
+class ModelAdmin(admin.ModelAdmin):
+	list_display = ('type', 'trademark', 'name', 'specifications',)
+	search_fields = ('type', 'trademark', 'name',)
+
+class DeviceAdmin(admin.ModelAdmin):
+	list_display = ('model', 'code', 'serial', 'part', 'ip', 'date_purchase', 'date_warranty', 'specifications')
+	search_fields = ('code', 'serial', 'part', 'ip')
+	filter_fields = ('model',)
+
+admin.site.register(Trademark, TrademarkAdmin)
+admin.site.register(Type, TypeAdmin)
+admin.site.register(Model, ModelAdmin)
+admin.site.register(Device, DeviceAdmin)
--- equipment/models.py
+++ equipment/models.py
@@ -1,17 +1,27 @@
-from django_pg import models
+from django.db import models
+from jsonfield import JSONField
 
 class Trademark(models.Model):
 	name = models.CharField(max_length=32)
 
+	def __unicode__(self):
+		return self.name
+
 class Type(models.Model):
 	name = models.CharField(max_length=32)
-	specifications = models.JSONField()
+	specifications = JSONField(blank=True, null=True)
+
+	def __unicode__(self):
+		return self.name
 
 class Model(models.Model):
 	name = models.CharField(max_length=128)
-	specifications = models.JSONField()
+	specifications = JSONField(blank=True, null=True)
 	type = models.ForeignKey(Type)
 	trademark = models.ForeignKey(Trademark)
+
+	def __unicode__(self):
+		return self.name
 
 class Device(models.Model):
 	code = models.CharField(max_length=16, unique=True)
@@ -20,5 +30,5 @@ class Device(models.Model):
 	ip = models.CharField(max_length=16, blank=True, null=True)
 	date_purchase = models.DateTimeField()
 	date_warranty = models.DateTimeField()
-	specifications = models.JSONField()
+	specifications = JSONField(blank=True, null=True)
 	model = models.ForeignKey(Model)
