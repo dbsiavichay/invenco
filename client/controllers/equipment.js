171f4ab5bcd5534e9af3e67dfa94964c319f68ad
Denis Siavichay <dbsiavichay@gmail.com>
2016-06-17 16:09:34 -0500

    Trademark angular template

--- client/controllers/equipment.js
+++ client/controllers/equipment.js
@@ -3,8 +3,12 @@
     'equipment.services'
   ])
 
-  .controller('TrademarkController', function ($scope, Trademark) {
+  .controller('TrademarkController', function ($location, $scope, Trademark) {
     $scope.objectList = Trademark.query();
-    
+    $scope.edit = false;
+
+    $scope.add = function () {
+      $scope.edit = true;
+    }
   });
 })();
