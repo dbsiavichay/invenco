1abc0f03d2628413338ae8b4e29b25b8f32d3ce7
Denis Siavichay <dbsiavichay@gmail.com>
2019-04-26 11:42:26 -0500

    Purchases

--- maintenance/urls.py
+++ maintenance/urls.py
@@ -13,6 +13,5 @@ urlpatterns = [
 
 
     url(r'^fix/$',login_required(FixListView.as_view()), name='fix_list'),
-    url(r'^fix/add/$', login_required(FixCreateView.as_view()), name='fix_create'),
-    #url(r'^brand/(?P<pk>\d+)/edit/$', login_required(BrandUpdateView.as_view()), name='brand_update'),    
+    url(r'^fix/add/$', login_required(FixCreateView.as_view()), name='fix_create'),    
 ]
