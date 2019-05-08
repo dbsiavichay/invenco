54593646713d2d3fba62398c14a73a52c1dfd745
Denis Siavichay <dbsiavichay@gmail.com>
2019-05-08 14:20:53 -0500

    Order scritps

--- maintenance/script.py
+++ maintenance/script.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 fixes = Fix.objects.all()
 
 for f in fixes:	
-	t = Ticket.objects.create(problem_type_id=4, equipment=f.equipment, problem=f.problem, status=3, user=f.user)
+	t = Ticket.objects.create(problem_type_id=1, equipment=f.equipment, problem=f.problem, status=3, user=f.user)
 	t.date=f.date_joined
 	t.save()
 	r = Reply.objects.create(ticket=t, description=f.solution)
--- stocktaking/script.py
+++ stocktaking/script.py
@@ -2,19 +2,19 @@ from stocktaking.models import Equipment, Model
 
 lst = [{'list': [122, 42, 59, 84, 88, 23, 97, 138, 126, 158], 'id': 1}, {'list': [43, 127], 'id': 2}, {'list': [30, 50, 92], 'id': 3}, {'list': [93], 'id': 4}, {'list': [94], 'id': 5}, {'list': [95], 'id': 6}, {'list': [140, 74, 2], 'id': 7}, {'list': [3], 'id': 8}, {'list': [103, 142, 66, 5], 'id': 9}, {'list': [104, 143, 6, 67], 'id': 10}, {'list': [105, 68, 7], 'id': 11}, {'list': [107, 145, 70, 9], 'id': 12}, {'list': [110, 150, 129, 12, 133], 'id': 14}, {'list': [121, 155, 83, 22], 'id': 15}, {'list': [123, 85, 24], 'id': 16}, {'list': [124, 86, 25], 'id': 17}, {'list': [108, 71, 10, 146], 'id': 13}, {'list': [111, 152, 73, 13, 135], 'id': 18}, {'list': [90], 'id': 19}, {'list': [27], 'id': 20}, {'list': [28, 48], 'id': 21}, {'list': [51, 31], 'id': 22}, {'list': [52, 32], 'id': 23}, {'list': [53, 33], 'id': 24}, {'list': [35], 'id': 25}, {'list': [36], 'id': 26}, {'list': [99, 17, 62], 'id': 27}, {'list': [100, 63], 'id': 28}, {'list': [101, 64], 'id': 29}, {'list': [75], 'id': 30}, {'list': [76], 'id': 31}, {'list': [78, 118], 'id': 32}, {'list': [79, 119], 'id': 33}, {'list': [80, 115], 'id': 34}, {'list': [81, 117], 'id': 35}, {'list': [130], 'id': 36}, {'list': [16], 'id': 37}, {'list': [55], 'id': 38}, {'list': [56], 'id': 39}, {'list': [57], 'id': 40}, {'list': [58], 'id': 41}, {'list': [147], 'id': 42}, {'list': [154], 'id': 43}, {'list': [156], 'id': 44}, {'list': [157], 'id': 45}, {'list': [151], 'id': 46}, {'list': [], 'id': 47}, {'list': [38], 'id': 48}, {'list': [], 'id': 49}, {'list': [134], 'id': 50}, {'list': [136], 'id': 51}, {'list': [21, 19], 'id': 52}]
 
-# eqs = Equipment.objects.all()
-# for e in eqs:		
-# 	s = {}
-# 	for key in e.specifications:
-# 		for l in lst:
-# 			intkey = int(key)
-# 			if intkey in l['list']:							
-# 				id = str(l['id'])
-# 				value = 'SI' if e.specifications[key] == True else 'NO' if e.specifications[key] == False else e.specifications[key]
-# 				s.update({id: value})
-# 				break;	
-# 	e.specifications.update(s)
-# 	e.save()
+eqs = Equipment.objects.all()
+for e in eqs:		
+	s = {}
+	for key in e.specifications:
+		for l in lst:
+			intkey = int(key)
+			if intkey in l['list']:							
+				id = str(l['id'])
+				value = 'SI' if e.specifications[key] == True else 'NO' if e.specifications[key] == False else e.specifications[key]
+				s.update({id: value})
+				break;	
+	e.specifications.update(s)
+	e.save()
 
 eqs = Model.objects.all()
 for e in eqs:		
@@ -30,6 +30,84 @@ for e in eqs:
 	e.specifications.update(s)
 	e.save()
 ###
+
+
+# from stocktaking.models import Type
+
+# types = Type.objects.all()
+# for t in types:
+# 	print('----'+t.name+'----')
+# 	for ts in t.type_specifications.all().order_by('order'):
+# 		if ts.widget=='separator':
+# 			print 'GRUPO:'+ ts.label
+# 		else:
+# 			print(ts.id, ts.label)
+
+
+# a1=[(1, u'Direcci\xf3n IP'),(122, u'Direcci\xf3n ip'),(42, u'Direcci\xf3n Ip'),(59, u'Direcci\xf3n ip'),(84, u'Direcci\xf3n ip'),(88, u'Direcci\xf3n ip'),(23, u'Direcci\xf3n Ip'),(97, u'Direcci\xf3n ip'),(138, u'Direcci\xf3n ip'),(126, u'Direcci\xf3n ip'),(158, u'IPs S.O.')]
+# a2=[(2, u'Direcci\xf3n MAC'),(43, u'Direcci\xf3n Mac'),(127, u'Direcci\xf3n mac')]
+# a3=[(3, u'N\xfamero de cartucho negro'),(30, u'C\xf3digo de cartucho negro'),(50, u'C\xf3digo de cartucho negro'),(92, u'Cartucho de toner')]
+# a4=[(4, u'M\xf3dulo de imagen (DRUM)'),(93, u'M\xf3dulo de imagen (DRUM)')]
+# a5=[(5, u'M\xf3dulo de fusor'),(94, u'M\xf3dulo de fusor')]
+# a6=[(6, u'Rodillo de transferencia'),(95, u'Rodillo de transferencia')]
+# a7=[(7, u'Unidad \xd3ptica'),(140, u'Unidad \xd3ptica'),(74, u'Unidad \xf3ptica'),(2, u'Unidad \xd3ptica')]
+# a8=[(8, u'Unidad Lectora'),(3, u'Unidad Lectora')]
+# a9=[(9, u'Nombre de procesador'),(103, u'Nombre'),(142, u'Nombre/#Parte'),(66, u'Nombre'),(5, u'Nombre')]
+# a10=[(10, u'Velocidad (GHz)'),(104, u'Velocidad (GHz)'),(143, u'Velocidad (GHz)'),(6, u'Velocidad (GHz)'),(67, u'Velocidad (GHz)')]
+# a11=[(11, u'Conjunto de instrucciones'),(105, u'Conjunto de instrucciones'),(68, u'Conjunto de instrucciones'),(7, u'Conjunto de instrucciones')]
+# a12=[(12, u'Tipo de RAM'),(107, u'Tipo'),(145, u'Tipo'),(70, u'Tipo'),(9, u'Tipo')]
+# a13=[(14, u'Tipo de conexi\xf3n'),(110, u'Tipo'),(150, u'Tipo'),(129, u'Tipo'),(12, u'Tipo'),(133, u'Tipo')]
+# a14=[(15, u'Sistema operativo'),(121, u'Sistema operativo'),(155, u'Sistema Operativo'),(83, u'Sistema operativo'),(22, u'Sistema opetarivo')]
+# a15=[(16, u'Nombre de usuario'),(123, u'Nombre de usuario'),(85, u'Nombre de usuario'),(24, u'Nombre de usuario')]
+# a16=[(17, u'Acceso remoto'),(124, u'Acceso remoto'),(86, u'Acceso remoto'),(25, u'Acceso remoto')]
+# a17=[(13, u'Capacidad RAM (GB)'),(108, u'Capacidad (GB)'),(71, u'Capacidad (GB)'),(10, u'Capacidad (GB)'),(146, u'Capacidad (GB) Instalada')]
+# a18=[(18, u'Capacidad Disco (GB)'),(111, u'Capacidad (Gb)'),(152, u'Capacidad Instalada(GB)'),(73, u'Capacidad (GB)'),(13, u'Capacidad (GB)'),(135, u'Capacidad (GB)')]
+# a19=[(19, u'Doble cara'),(90, u'Duplex')]
+# a20=[(20, u'Tipo de impresi\xf3n'),(27, u'Tipo')]
+# a21=[(21, u'Tipo de suministro'),(28, u'Suministro'),(48, u'Suministro')]
+# a22=[(22, u'N\xfamero de cartucho cyan'),(51, u'C\xf3digo de cartucho cyan'),(31, u'C\xf3digo de cartucho cyan')]
+# a23=[(23, u'N\xfamero de cartucho magenta'),(52, u'C\xf3digo de cartucho magenta'),(32, u'C\xf3digo de cartucho magenta')]
+# a24=[(24, u'N\xfamero de cartucho amarillo'),(53, u'C\xf3digo de cartucho amarillo'),(33, u'C\xf3digo de cartucho amarillo')]
+# a25=[(25, u'N\xfamero de cartucho cyan claro'),(35, u'C\xf3digo de cartucho cyan claro')]
+# a26=[(26, u'N\xfamero de cartucho magenta claro'),(36, u'C\xf3digo de cartucho magenta claro')]
+# a27=[(27, u'Tama\xf1o de pantalla (plg)'),(99, u'Tama\xf1o (plg)'),(17, u'Dimensi\xf3n (Plg)'),(62, u'Tama\xf1o (plg)')]
+# a28=[(28, u'Resoluci\xf3n de pantalla'),(100, u'Resoluci\xf3n'),(63, u'Resoluci\xf3n')]
+# a29=[(29, u'Nombre de tarjeta de v\xeddeo'),(101, u'Tarjeta de video'),(64, u'Tarjeta de video')]
+# a30=[(30, u'Camar\xe1 web'),(75, u'Webcam')]
+# a31=[(31, u'Tecnolog\xeda de bateria'),(76, u'Tecnolog\xeda de bateria')]
+# a32=[(32, u'WIFI'),(78, u'WIFI'),(118, u'Wifi')]
+# a33=[(33, u'Bluetooth'),(79, u'Bluetooth'),(119, u'Bluetooth')]
+# a34=[(34, u'Cantidad de puertos USB'),(80, u'Cantidad de puertos USB'),(115, u'Cantidad de puertos usb')]
+# a35=[(35, u'HDMI'),(81, u'HDMI'),(117, u'HDMI')]
+# a36=[(36, u'Serial del sistema operativo'),(130, u'Serial del sistema operativo')]
+# a37=[(37, u'Tipo pantalla'),(16, u'Tipo')]
+# a38=[(38, u'N\xfamero de cabezal negro'),(55, u'C\xf3digo de cabezal negro')]
+# a39=[(39, u'N\xfamero de cabezal cyan'),(56, u'C\xf3digo de cabezal cyan')]
+# a40=[(40, u'N\xfamero de cabezal magenta'),(57, u'C\xf3digo de cabezal magenta')]
+# a41=[(41, u'N\xfamero de cabezal amarillo'),(58, u'C\xf3digo de cabezal amarillo')]
+# a42=[(42, u'Memoria expandible hasta (GB)'),(147, u'Capacidad(GB)')]
+# a43=[(43, u'S. Virtual'),(154, u'S. Virtual')]
+# a44=[(44, u'Direcci\xf3n IP IOS'),(156, u'IP IOS')]
+# a45=[(45, u'Direcci\xf3n IP Virtual'),(157, u'IP Virtual')]
+# a46=[(46, u'Numero de discos/capacidad(GB)'),(151, u'Cantidad/capacidad')]
+# a47=[(47, u'Nombre en pantalla')]
+# a48=[(48, u'N\xfamero de telefono'),(38, u'Numero de extensi\xf3n')]
+# a49=[(49, u'Contrase\xf1a telefono')]
+# a50=[(50, u'Tama\xf1o de disco (plg)'),(134, u'Tama\xf1o')]
+# a51=[(51, u'Rendimiento del disco (RPM)'),(136, u'Rendimiento (RPM)')]
+# a52=[(52, u'Conector mouse'),(21, u'Conector'),(19, u'Conector')]
+
+# lst = []
+# for i in range(1,53):	
+# 	a = eval('a'+str(i))
+# 	_id = a.pop(0)[0]
+# 	_list =  [t[0] for t in a]
+# 	lst.append({
+# 		'id': _id,
+# 		'list': _list
+# 	})
+
+# print lst
 
 
 # (1, u'Direcci\xf3n IP') - (122, u'Direcci\xf3n ip') (42, u'Direcci\xf3n Ip') (59, u'Direcci\xf3n ip') (84, u'Direcci\xf3n ip') (88, u'Direcci\xf3n ip') (23, u'Direcci\xf3n Ip') (97, u'Direcci\xf3n ip') (138, u'Direcci\xf3n ip') (126, u'Direcci\xf3n ip') (158, u'IPs S.O.')
